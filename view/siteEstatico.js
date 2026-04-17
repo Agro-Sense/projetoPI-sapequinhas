c711f95507499f0e8dcf920f1142bf9adb9035d0
GiuliaTeixeira3626 <giulia.teixeira@sptech.school>
2026-04-17 13:42:43 -0300

    Adicionando Site Estático de Cadastro e Login

---
+++ view/siteEstatico.js
@@ -0,0 +1,44 @@
+var listaUsuarios = [];
+var listaSenhas = [];
+var listaEmpresas = [];
+
+function Registrar() {
+    var empresa = ipt_Empresa.value;
+    var user = ipt_User.value.toLowerCase();
+    var senha = ipt_Senha_User.value;
+    var msg_erro = "";
+
+    if (empresa == "" || user == "" || senha == "") {
+        msg_erro = "<p id='msg_alerta'>Preencha todos os campos!</p><br>";
+    } else if (!user.includes('@')) {
+        msg_erro = "<p id='msg_alerta'>Usuário deve conter @!</p><br>";
+    } else if (senha.length < 8) {
+        msg_erro = " <p id='msg_alerta'>Senha deve ter 8+ caracteres!</p><br>";
+    }
+
+    if (msg_erro != "") {
+        div_msg.innerHTML = msg_erro;
+    } else {
+        listaUsuarios.push(user);
+        listaSenhas.push(senha);
+        listaEmpresas.push(empresa);
+
+        div_msg.innerHTML = " <p id='msg_alerta'>Cadastro realizado! Agora você pode fazer login.</p>";
+        ipt_Empresa.value = "";
+        ipt_User.value = "";
+        ipt_Senha_User.value = "";
+    }
+}
+
+function Validar() {
+    var userLogin = ipt_Login_User.value.toLowerCase();
+    var senhaLogin = ipt_Login_Senha.value;
+
+    var indice = listaUsuarios.indexOf(userLogin);
+
+    if (indice != -1 && listaSenhas[indice] == senhaLogin) {
+        div_msg.innerHTML = ` <p id='msg_alerta'>Login realizado! Bem-vindo(a) da empresa</p>  ${listaEmpresas[indice]}`;
+    } else {
+        div_msg.innerHTML = " <p id='msg_alerta'>Usuário ou senha incorretos!</p>";
+    }
+}
